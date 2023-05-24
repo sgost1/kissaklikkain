0d205ee1fdccab5d6189038e8b1ddd1196a87917
sgost1 <103131559+sgost1@users.noreply.github.com>
2023-05-24 11:28:15 +0300

    Pieniä korjauksia

--- app.js
+++ app.js
@@ -43,26 +43,19 @@ function lisaaKissanMaito() {
     document.getElementById("kissatSekunnissa").innerHTML = kissatSekunnissa + " /s"
 }
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
+// Päivitysvalikko
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
+// Avaa valikko kun käyttäjä painaa nappia
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
+// Jos käyttäjä painaa X merkkiä niin valikko sulkeutuu
 span.onclick = function() {
   modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
+// Jos käyttäjä klikkaa valikon ulkopuolelta niin valikko sulkeutuu
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
